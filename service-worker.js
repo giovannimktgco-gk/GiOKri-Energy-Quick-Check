2a77be0ea678ef26280e5c7b5a33272d45df52d3
giovannimktgco-gk <giovanni.mktgco@gmail.com>
2026-07-24 16:09:17 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE = "giokri-energy-v1";
+
+const FILES = [
+"index.html",
+"style.css",
+"app.js"
+];
+
+
+self.addEventListener(
+"install",
+event => {
+
+event.waitUntil(
+
+caches.open(CACHE)
+.then(cache =>
+cache.addAll(FILES)
+)
+
+);
+
+});
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response =>
+response || fetch(event.request)
+)
+
+);
+
+});
