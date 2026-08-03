b80ded7a8277a87185a6b35e8a3279c13bcb90ff
giovannimktgco-gk <giovanni.mktgco@gmail.com>
2026-08-03 16:35:07 +0200

    Enhance service worker with installation and activation logic
    Updated service worker to include manifest.json and handle activation.

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,104 @@
-const CACHE = "giokri-energy-v2";
+const CACHE = "giokri-energy-v2.1";
 
 const FILES = [
 "index.html",
 "style.css",
-"app.js"
+"app.js",
+"manifest.json"
 ];
 
+
+
+/****************************************************
+ * INSTALLAZIONE
+ ****************************************************/
 
 self.addEventListener(
 "install",
 event => {
 
-event.waitUntil(
+  self.skipWaiting();
 
-caches.open(CACHE)
-.then(cache =>
-cache.addAll(FILES)
-)
 
-);
+  event.waitUntil(
+
+    caches.open(CACHE)
+
+    .then(cache =>
+
+      cache.addAll(FILES)
+
+    )
+
+  );
 
 });
 
+
+
+
+/****************************************************
+ * ATTIVAZIONE
+ ****************************************************/
+
+self.addEventListener(
+"activate",
+event => {
+
+
+  event.waitUntil(
+
+    caches.keys()
+
+    .then(keys =>
+
+      Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE){
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      )
+
+    )
+
+  );
+
+
+});
+
+
+
+
+
+/****************************************************
+ * GESTIONE RICHIESTE
+ ****************************************************/
 
 self.addEventListener(
 "fetch",
 event => {
 
+
 event.respondWith(
 
-caches.match(event.request)
-.then(response =>
-response || fetch(event.request)
-)
+
+  caches.match(event.request)
+
+  .then(response =>
+
+    response || fetch(event.request)
+
+  )
+
 
 );
+
 
 });
